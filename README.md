621f76804b9c4e4d84597aec9ae480494d05a17c
shininome <gtx2shino@gmail.com>
2024-04-22 12:21:57 +0800

    a

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-# 不器小窝
+# 喵喵喵喵
 
 又一个个人网站，虽然记录的都比较浅显，但仍不失为自己的一个站点。
-
-## 说明
-
-麻烦`forks`的兄弟们把统计信息和广告信息清理掉一下，谢谢
 
 ## 期望
 
 但愿`GitHub`别倒闭了，让我可以拥有免费的网站服务器，能记录下我的想写想说。
 
-地址：[不器小窝](https://xingcxb.com)
+地址：[不器小窝](https://shininome.github.io/shinonomeow.github.io/ )
 
-## ⭐`Star 不器小窝`
 
-[![Stargazers over time](https://starchart.cc/xingcxb/xingcxb.github.io.svg)](https://starchart.cc/xingcxb/xingcxb.github.io)
--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,9 @@ npm run build
 cd docs/.vuepress/dist
 
 # deploy to github pages
-# echo 'xingcxb.com' > CNAME
+echo 'https://shininome.github.io/shinonomeow.github.io/ ' > CNAME
 
-echo 'google.com, pub-1725717718088510, DIRECT, f08c47fec0942fa0' > ads.txt
+echo 'google.com,  DIRECT, f08c47fec0942fa0' > ads.txt
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
@@ -27,19 +27,6 @@ git init
 git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
-
-# deploy to coding pages
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
