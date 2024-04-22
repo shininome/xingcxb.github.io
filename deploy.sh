80145c823844cc3b14c13a16438e9e8dedc25771
shininome <gtx2shino@gmail.com>
2024-04-22 12:05:10 +0800

    a

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,43 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-
-push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
-
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
-cd $dist_path
+cd docs/.vuepress/dist
 
+# deploy to github pages
+# echo 'xingcxb.com' > CNAME
+
+echo 'google.com, pub-1725717718088510, DIRECT, f08c47fec0942fa0' > ads.txt
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:shininome/shinonomeow.github.io.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://shininome:${GITHUB_TOKEN}@github.com/shininome/shinonomeow.github.io.git
+  git config --global user.name "shininome"
+  git config --global user.email "gtx2shino@gmail.com"
+fi
 git init
 git add -A
-git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+
+# deploy to coding pages
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:xgy/xgy.git
+# else
+#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
 cd -
-rm -rf $dist_path
+rm -rf docs/.vuepress/dist
